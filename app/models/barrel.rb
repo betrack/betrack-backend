459a703f6f92d2dadc12d3151648a3749fc3a06b
jota <juanbanelli@gmail.com>
2016-10-12 16:38:40 -0300

    Showing barrel last temperature

--- app/models/barrel.rb
+++ app/models/barrel.rb
@@ -2,4 +2,8 @@ class Barrel < ApplicationRecord
   has_many :barrel_statuses
   belongs_to :store, :optional => true
   belongs_to :transport, :optional => true
+
+  def last_temperature
+    self.barrel_statuses.last.try(:temperature) || '-'
+  end
 end
