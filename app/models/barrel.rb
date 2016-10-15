b0b30edc71dacaf0e9b803d5a18bf71b99074780
jota <juanbanelli@gmail.com>
2016-10-15 18:55:43 -0300

    Enhanced index view for barrels

--- app/models/barrel.rb
+++ app/models/barrel.rb
@@ -25,4 +25,18 @@ class Barrel < ApplicationRecord
       .last(10)
   end
 
+  def owner_type
+    if self.transport
+      'transport'
+    elsif self.store
+      'store'
+    else
+      '-'
+    end
+  end
+
+  def battery_level
+    self.barrel_statuses.where('battery_level is not null').last.try(:battery_level)
+  end
+
 end
